82c31f608cddc4d6524bb599954f8cb25e8b6080
Vincent Le <49280437+VincentLeV@users.noreply.github.com>
2021-04-20 16:11:55 +0300

    Add http

--- index.js
+++ index.js
@@ -1,7 +1,83 @@
-const Logger = require( "./logger" );
+const http = require( "http" );
+const path = require( "path" );
+const fs = require( "fs" );
 
-const logger = new Logger();
+const server = http.createServer( (req, res) => {
+    // if ( req.url === "/" ) {
+    //     fs.readFile( path.join(__dirname, "public", "index.html"), (err, content) => {
+    //         if ( err ) throw err;
+    //         res.writeHead( 200, {"Content-Type": "text/html"} );
+    //         res.end( content );
+    //     })
+    // }
 
-logger.on( "message", data => console.log("Called Listener", data) );
+    // if ( req.url === "/about" ) {
+    //     fs.readFile( path.join(__dirname, "public", "about.html"), (err, content) => {
+    //         if ( err ) throw err;
+    //         res.writeHead( 200, {"Content-Type": "text/html"} );
+    //         res.end( content );
+    //     })
+    // }
 
-logger.log( "Hello World!" );
+    // if ( req.url === "/api/users" ) {
+    //     const users = [
+    //         { name: "Bob Smith", age: 40 },
+    //         { name: "John Doe", age: 30 }
+    //     ]
+    //     res.writeHead( 200, {"Content-Type": "application/json"} );
+    //     res.end( JSON.stringify(users) );
+    // }
+
+    // Build file path
+    let filePath = path.join( __dirname, "public", req.url === "/" ? "index.html" : req.url );
+
+    // Extension of file
+    let extName = path.extname( filePath );
+
+    // Initial content type
+    let contentType = "text/html";
+
+    // Check extension -> set contentType
+    switch( extName ) {
+        case ".js":
+            contentType = "text/javascript";
+            break;
+        case ".css":
+            contentType = "text/css";
+            break;
+        case ".json":
+            contentType = "application/json";
+            break;
+        case ".png":
+            contentType = "image/png";
+            break;
+        case ".jpg":
+            contentType = "image/jpg";
+            break;
+    }
+
+    // Read file
+    fs.readFile( filePath, (err, content) => {
+        if ( err ) {
+            if ( err.code == "ENONET" ) {
+                // Page not found
+                fs.readFile( path.join( __dirname, "public", "404.html" ), (err, content) => {
+                    res.writeHead( 200, {"Content-Type": "text/html"} );
+                    res.end( content, "utf8" );
+                })
+            } else {
+                // Some server error
+                res.writeHead( 500 );
+                res.end( `Server error: ${err.code}` );
+            }
+        } else {
+            // Success
+            res.writeHead( 200, {"Content-Type": contentType} );
+            res.end( content, "utf8" );
+        }
+    });
+});
+
+const port = process.env.port || 5000;
+
+server.listen( port, () => console.log(`Server is running on port ${port}`));
